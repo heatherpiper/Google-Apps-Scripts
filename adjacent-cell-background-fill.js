793ed4bf0edac3d6598adfd72c0205ef00bf6d10
heatherpiper <heather.a.piper@gmail.com>
2023-09-21 22:08:17 -0400

    Allow user to input range

--- adjacent-cell-background-fill.js
+++ adjacent-cell-background-fill.js
@@ -1,15 +1,31 @@
-// This script will copy the hex string values in a given column and then change the background fill of the adjacent cell to match that value. Replace or modify the cell range and column number to suit your needs.
+// This script will copy the hex string values from one column and then set the background color of the adjacent cells to the corresponding hex value. A dialog box will prompt you for the range of cells that contain the hex codes.
 
-function onEdit() {
+function setAdjacentBackgroundColors() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var ui = SpreadsheetApp.getUi();
 
-  var colorCodes = sheet.getRange("C1:C247").getValues();
+  var result = ui.prompt('Enter the range of cells that contain hex values. NOTE: Cells must be within the same column.');
+  var rangeInput = result.getResponseText();
+
+  if (rangeInput === "") {
+    ui.alert('Please enter a valid range.');
+    return;
+  }
+
+  var colorCodesRange = sheet.getRange(rangeInput);
+  
+  var startingRow = colorCodesRange.getRow();
+  var startingCol = colorCodesRange.getColumn();
+  var colorCodes = colorCodesRange.getValues();
 
   for (var i = 0; i < colorCodes.length; i++) {
     var colorCode = colorCodes[i][0];
 
     if (isHex(colorCode)) {
-      sheet.getRange(i + 1, 4).setBackground(colorCode);
+      var targetRow = startingRow + i;
+      var targetCol = startingCol + 1;
+
+      sheet.getRange(targetRow, targetCol).setBackground(colorCode);
     }
   }
 }
